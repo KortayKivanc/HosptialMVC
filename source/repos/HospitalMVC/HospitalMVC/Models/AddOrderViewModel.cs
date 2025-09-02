dc5e39c769ea83795e57aa444c37b098fd71205e
Kortay <kortaykokturk@gmail.com>
2025-09-02 17:10:53 +0300

    Delete source/repos/HospitalMVC directory

--- source/repos/HospitalMVC/HospitalMVC/Models/AddOrderViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HospitalMVC.Models
-{
-	public class AddOrderViewModel
-	{
-		public int PatientId { get; set; }
-		public string TestName { get; set; } = "";
-		public string Results { get; set; } = "";
-	}
-}
--- source/repos/HospitalMVC/HospitalMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
